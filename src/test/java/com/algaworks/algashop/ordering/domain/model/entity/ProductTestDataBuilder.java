af4fa925ded36850b75cb05b9fbbfd85454645ea
abilioneto <abilio@bsd.com.br>
2025-09-09 22:16:20 -0300

    implement shopping cart

--- src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ProductTestDataBuilder.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ProductTestDataBuilder.java
@@ -1,7 +1,7 @@
 package com.algaworks.algashop.ordering.domain.model.entity;
 
-import com.algaworks.algashop.ordering.domain.model.valueobject.Product;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Product;
 import com.algaworks.algashop.ordering.domain.model.valueobject.ProductName;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.ProductId;
 
@@ -22,7 +22,7 @@ public class ProductTestDataBuilder {
 
     public static Product.ProductBuilder aProductUnavailable() {
         return Product.builder()
-                .id(DEFAULT_PRODUCT_ID)
+                .id(new ProductId())
                 .name(new ProductName("Desktop FX9000"))
                 .price(new Money("5000"))
                 .inStock(false);
@@ -30,7 +30,7 @@ public class ProductTestDataBuilder {
 
     public static Product.ProductBuilder aProductAltRamMemory() {
         return Product.builder()
-                .id(DEFAULT_PRODUCT_ID)
+                .id(new ProductId())
                 .name(new ProductName("4GB RAM"))
                 .price(new Money("200"))
                 .inStock(true);
@@ -38,7 +38,7 @@ public class ProductTestDataBuilder {
 
     public static Product.ProductBuilder aProductAltMousePad() {
         return Product.builder()
-                .id(DEFAULT_PRODUCT_ID)
+                .id(new ProductId())
                 .name(new ProductName("Mouse Pad"))
                 .price(new Money("100"))
                 .inStock(true);
--- src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCartTestDataBuilder.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/entity/ShoppingCartTestDataBuilder.java
@@ -1,13 +1,12 @@
-
 package com.algaworks.algashop.ordering.domain.model.entity;
 
-import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.ShoppingCartId;
 
 public class ShoppingCartTestDataBuilder {
 
-    public CustomerId customerId = new CustomerId();
+    public CustomerId customerId = CustomerTestDataBuilder.DEFAULT_CUSTOMER_ID;
     public static final ShoppingCartId DEFAULT_SHOPPING_CART_ID = new ShoppingCartId();
     private boolean withItems = true;
 
@@ -44,4 +43,4 @@ public class ShoppingCartTestDataBuilder {
         this.withItems = withItems;
         return this;
     }
-}
+} 
